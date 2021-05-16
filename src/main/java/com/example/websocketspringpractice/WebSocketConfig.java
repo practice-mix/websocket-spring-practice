7106da9adfe0dce4a18d02f9e9d94ef9015e4c61
LeonGeroge <leongeroge@gmail.com>
2021-05-16 16:12:46 +0800

    allow cross origin

--- src/main/java/com/example/websocketspringpractice/WebSocketConfig.java
+++ src/main/java/com/example/websocketspringpractice/WebSocketConfig.java
@@ -52,7 +52,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry
+                .addEndpoint("/gs-guide-websocket")
+                .setAllowedOrigins("*")
+                .withSockJS();
 
     }
 
--- src/main/java/com/example/websocketspringpractice/chat/package-info.java
+++ src/main/java/com/example/websocketspringpractice/chat/package-info.java
@@ -1,6 +1,12 @@
 /**
  * websocket + rabbitmq
  * <p>
+ * {@code
+ * rabbitmq-plugins enable rabbitmq_stomp
+ * }
+ * </p>
+ * <p>
  * https://www.javainuse.com/spring/boot-websocket-chat
+ * </p>
  */
 package com.example.websocketspringpractice.chat;
